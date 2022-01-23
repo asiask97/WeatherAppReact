ea1f11480740e579d6f0e507f00cee8cb53f73e9
asiask97 <joannaskoc@gmail.com>
2022-01-23 17:58:02 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState , useEffect , useCallback} from 'react'
 
 function App() {
+  const key = process.env.REACT_APP_WEATHER_API_KEY
+  const [ip, setIP] = useState('');
+  const [currentTep, setCurrentTemp] = useState('')
+  
+  const fetchIP = useCallback(async () => {
+    console.log('ip fetch')
+    const resp = await fetch('https://geolocation-db.com/json/')
+    const json = await resp.json()
+    setIP(json.IPv4)
+  }, []) 
+
+  const fetchWeather = useCallback(async () => {
+      console.log('wether Fetch')
+    try{
+      //check if ip was fetched
+      if(ip == ''){
+        throw new Error("No ip was found to display weather")
+      }
+      //fetch weather data 
+      const resp = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${ip}&aqi=yes`)
+      
+      //check for correct satus
+      if(!resp.ok){
+        throw new Error(resp.statusText)
+      }
+      else{
+        const json = await resp.json()
+        setCurrentTemp(json.current.temp_c)
+      }
+    }
+    catch(e){
+      console.log(e.message)
+    }
+  }, [ip])
+
+  useEffect( () => {
+    fetchIP()
+    fetchWeather()
+    //empty array cause i want it to run only once
+  }, [fetchIP, fetchWeather])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Your IP Address is</h2>
+        <h4>{ip}</h4>
+        <h2>Your Temperture is </h2>
+        <h4>{currentTep && currentTep}*C</h4>
     </div>
   );
 }
