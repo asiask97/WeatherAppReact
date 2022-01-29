164382470b0c342ce559493a99ae106af1080037
asiask97 <joannaskoc@gmail.com>
2022-01-29 14:01:12 +0000

    Fixed day of the week bug

--- src/components/CurrentBasics.js
+++ src/components/CurrentBasics.js
@@ -5,7 +5,7 @@ export default function CurrentBasics(prop) {
     return(
         <div className="currentBasics">
             <h3>{prop.location}</h3>
-            <h2>{prop.temp}°C</h2>
+            <h2>{Math.round(prop.temp)}°C</h2>
             <WeatherIcon condition={prop.conditionCode}/>
             <h4>{prop.condition}</h4>
         </div>
--- src/components/ForcastDay.js
+++ src/components/ForcastDay.js
@@ -3,11 +3,8 @@ import WeatherIcon from './WeatherIcon';
 
 export default function ForcastDay(props) {
     const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    const today = "Today"
-    const now = new Date();
-    let day = days[ now.getDay()+props.day];
-    if(props.day === 0){day = today}
-    
+    let day = days[ props.day];
+
     return(
         <div className="forcastDay">
             <p>{day}</p>
